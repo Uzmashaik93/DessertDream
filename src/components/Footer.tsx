2ac712293eaa0d02810ba05a37b25393baacddce
uzma shaik <uzma97910@gmail.com>
2025-04-14 12:32:15 +0200

    feat: update Footer component with new styling and copyright information

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 
-interface FooterProps {}
-
-const Footer: React.FC<FooterProps> = () => {
+const Footer: React.FC = () => {
   return (
-    <footer className="bg-gray-100 text-center p-4">
-      <p>&copy; 2024 HomeBaker Delights</p>
+    <footer className="bg-white/30 backdrop-blur-md text-center p-4 flex justify-between rounded-lg">
+      <div>
+        <p>
+          Copyright &copy; 2024 Dessert Dream by Namita. All Rights Reserved.
+        </p>
+      </div>
+      <div className="flex gap-20">
+        <div>Terms & Conditions</div>
+        <div>Site Map</div>
+      </div>
     </footer>
   );
 };
