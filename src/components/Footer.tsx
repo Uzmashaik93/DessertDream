93be404f71fcf2c4d4bf93baa7659251702deb9c
uzma shaik <uzma97910@gmail.com>
2025-04-14 19:07:37 +0200

    fix: update .gitignore to include .env; modify Footer component for improved styling and link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,13 +3,18 @@ import React from "react";
 const Footer: React.FC = () => {
   return (
     <footer className="bg-white/30 backdrop-blur-md text-center p-4 flex flex-col md:flex-row justify-between items-center gap-4 rounded-lg">
-      <div className="text-sm text-gray-700">
+      <div className="text-sm font-bold text-gray-700">
         <p>© 2024 Dessert Dream by Namita. All Rights Reserved.</p>
       </div>
 
       <div className="flex flex-col md:flex-row gap-2 md:gap-10 text-sm text-pink-700 font-medium">
-        <div className="hover:underline cursor-pointer">Terms & Conditions</div>
-        <div className="hover:underline cursor-pointer">Site Map</div>
+        <a
+          href="https://github.com/Uzmashaik93"
+          target="_blank"
+          className="hover:cursor-pointer hover:text-pink-500 font-bold"
+        >
+          Created By Uzma
+        </a>
       </div>
     </footer>
   );
